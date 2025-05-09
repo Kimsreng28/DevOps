b4ebc67f6d2c488b10042ed863b06098b5d9463c
Kimsreng28 <kimsrenghai@gmail.com>
2025-05-09 10:04:13 +0700

    Spring gradle

---
+++ src/main/java/gic/i4/library_webapp_haikimsreng/LibraryWebappHaikimsrengApplication.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_haikimsreng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebappHaikimsrengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebappHaikimsrengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gic/i4/library_webapp_haikimsreng/LibraryWebappHaikimsrengApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_haikimsreng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebappHaikimsrengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
